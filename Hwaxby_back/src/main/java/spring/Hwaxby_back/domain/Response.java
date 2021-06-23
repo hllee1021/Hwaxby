2d224c8cb7989b90510d0b7c3323af5c05566e3c
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-06-23 22:42:25 +0900

    [revise] Response Domain architecture

--- Hwaxby_back/src/main/java/spring/Hwaxby_back/domain/Response.java
+++ Hwaxby_back/src/main/java/spring/Hwaxby_back/domain/Response.java
@@ -2,6 +2,7 @@ package spring.Hwaxby_back.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import spring.Hwaxby_back.domain.OpenWeather.OpenWeather;
 
 @Getter @Setter
 public class Response {
@@ -9,12 +10,14 @@ public class Response {
     private Long id;
 
     // 가공한 데이터
-    private Voice voice;
-    private Display apiData;
+    private Voice voice; // Type : Response
+    private Display displayData;
+    private OpenWeather apiData;
+    private OpenWeatherType type;
 
-    // 제공받은 데이터
-    private Float lat;
-    private Float lon;
-    private String city;
+//    // 제공받은 데이터
+//    private Float lat;
+//    private Float lon;
+//    private String city;
 
 }
