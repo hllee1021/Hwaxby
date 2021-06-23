8525cad2e49baa7ea65fc794d419fbda5ee82a51
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-06-24 01:11:41 +0900

    [add] geocoder func.

--- Hwaxby_back/src/main/java/spring/Hwaxby_back/service/WeatherService.java
+++ Hwaxby_back/src/main/java/spring/Hwaxby_back/service/WeatherService.java
@@ -1,13 +1,22 @@
 package spring.Hwaxby_back.service;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonArray;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import spring.Hwaxby_back.domain.Coordinates;
 import spring.Hwaxby_back.domain.OpenWeather.CurrentWeather;
 import spring.Hwaxby_back.domain.OpenWeather.ForecastWeather;
 import spring.Hwaxby_back.domain.OpenWeather.OpenWeather;
 import spring.Hwaxby_back.domain.OpenWeatherType;
 import spring.Hwaxby_back.util.PropertyFileReader;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Properties;
 
@@ -66,6 +75,64 @@ public class WeatherService {
         return response;
     }
 
+    public Coordinates geocoder(String city) throws Exception {
+//        StringBuffer sb = new StringBuffer();
+
+        Properties prop = PropertyFileReader.readPropertyFile("api-key.properties");
+        String clientId = prop.getProperty("naver.api.accessID");
+        String clientSecret = prop.getProperty("naver.api.accesskey");
+
+        try{
+            String addr = URLEncoder.encode(city, "utf-8");
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + addr;
+
+            URL url = new URL(apiURL);
+            HttpsURLConnection http = (HttpsURLConnection) url.openConnection();
+            http.setRequestMethod("GET");
+            http.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            http.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            int responseCode = http.getResponseCode();
+
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(http.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            br.close();
+
+            System.out.println(response);
+
+//            System.out.println("code:"+responseCode + ":" + response.toString());
+//            InputStreamReader in = new InputStreamReader(http.getInputStream(), "utf-8");
+//            String line;
+//
+//            while ((line = br.readLine()) != null){
+//                sb.append(line).append("\n");
+//            }
+
+//            JSONParser parser = new JSONParser();
+//            JSONPObject jsonObject1;
+//            JSONPObject jsonObject2;
+//            JsonArray jsonArray;
+//            String x = "";
+//            String y = "";
+//
+//            jsonObject1 = (JSONPObject) parser.parse(sb.toString());
+//            jsonArray = (JsonArray) jsonObject1.get("addresses");
+//            for (int i = 0 ; j < jsonArray.size() ; )
+        } catch (IOException e){
+            System.out.println(e);
+        }
+        return null;
+    }
 
 }
 
