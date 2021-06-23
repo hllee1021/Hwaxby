70ebebc432a3743f4b404ebac8b3ea977f99eec0
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-06-23 16:15:23 +0900

    [add] Coordinates Domain

---
+++ Hwaxby_back/src/main/java/spring/Hwaxby_back/domain/Coordinates.java
@@ -0,0 +1,13 @@
+package spring.Hwaxby_back.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Coordinates {
+    private Long id;
+
+    private Float lat;
+    private Float lon;
+
+}
