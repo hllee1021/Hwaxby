7c3059e81159d6b3eb2a9edf0ada1656ab8302b5
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-06-24 03:43:16 +0900

    [update] response API - insert geocoder

--- Hwaxby_back/src/main/java/spring/Hwaxby_back/controller/ResponseController.java
+++ Hwaxby_back/src/main/java/spring/Hwaxby_back/controller/ResponseController.java
@@ -11,6 +11,7 @@ import spring.Hwaxby_back.domain.*;
 import spring.Hwaxby_back.domain.OpenWeather.CurrentWeather;
 import spring.Hwaxby_back.domain.OpenWeather.ForecastWeather;
 import spring.Hwaxby_back.domain.OpenWeather.OpenWeather;
+import spring.Hwaxby_back.domain.VoiceItem.TextParsed;
 import spring.Hwaxby_back.service.CoordService;
 import spring.Hwaxby_back.service.VoiceService;
 import spring.Hwaxby_back.service.WeatherService;
@@ -22,22 +23,34 @@ public class ResponseController {
 
     private final VoiceService voiceService;
     private final WeatherService weatherService;
+    private final CoordService coordService;
 
     @Autowired
-    public ResponseController(VoiceService voiceService, WeatherService weatherService) {
+    public ResponseController(VoiceService voiceService, WeatherService weatherService, CoordService coordService) {
         this.voiceService = voiceService;
         this.weatherService = weatherService;
+        this.coordService = coordService;
     }
 
     @PostMapping("response")
     public ResponseEntity<?> getResponse(@RequestBody Ask askData) throws Exception {
         // [TEMP] tester
-        System.out.println("here");
+        System.out.println("/response API called");
 //        Coordinates test_cor = new Coordinates();
 //        test_cor.setLat(36.504658); test_cor.setLon(129.44539);
 //        askData.setCoordinates(test_cor);
-        OpenWeatherType type;
+
 //        type = OpenWeatherType.CURRENT;
+
+        OpenWeatherType type;
+        Optional<Coordinates> opcoor = coordService.findOne(askData.getCoordinates().getId());
+        Coordinates coordinates = null;
+
+        if (opcoor.isPresent()) {
+            coordinates = opcoor.get();
+        } else {
+            System.out.println("There's no Coordinates entity match to id: "+ askData.getCoordinates().getId());
+        }
 
         /** 0. Response 객체 생성 */
         Response response = new Response();
@@ -55,8 +68,16 @@ public class ResponseController {
 
 
         /** 2. 1번 결과로부터 Keyword 추출 */
+        // OpenWeatherType (CURRENT, FORECAST) 추출
         type = voice.getTextParsed().getOpenWeatherType();
         response.setType(type);
+
+        // city(지역)이 주어진 경우 -> geocoder 함수 호출 -> Coordinates 추출
+        if (!voice.getTextParsed().getCity().equals("HERE")){ // city가 HERE이 아니다 = 별도의 city 요청
+            Coordinates city_coor = weatherService.geocoder(voice.getTextParsed().getCity());
+            coordinates.setLat(city_coor.getLat());
+            coordinates.setLon(city_coor.getLon());
+        }
 
 
         /** 3. 2번 결과로부터 Keyword에 대한 (Current, Forecast) 분기  -> OpenWeather API */
@@ -68,7 +89,7 @@ public class ResponseController {
             api_result = new ForecastWeather();
         }
 
-        response.setApiData(weatherService.getCurrentByCoor(api_result, type, askData.getCoordinates().getLat(), askData.getCoordinates().getLon()));
+        response.setApiData(weatherService.getCurrentByCoor(api_result, type, coordinates.getLat(), coordinates.getLon()));
 
         /** 4. Model Input 문장 생성 및 Model 요청 -> Voice(Type:Response) 객체 생성*/
 
@@ -80,11 +101,11 @@ public class ResponseController {
         /** 7. return */
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-    
+
     @GetMapping("test")
-    public ResponseEntity<?> tester(@RequestBody Ask askData) throws Exception {
+    public ResponseEntity<?> tester(@RequestBody TextParsed askData) throws Exception {
         System.out.println("testing");
-        Coordinates response = weatherService.geocoder("영덕");
+        Coordinates response = weatherService.geocoder(askData.getCity());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
