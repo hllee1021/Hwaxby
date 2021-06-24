5ca28f93db39f8fb358cb795910d8b66c06c53e0
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-06-24 19:37:41 +0900

    음성 16000 완료

--- Client/android/app/src/main/java/com/client/MainApplication.java
+++ Client/android/app/src/main/java/com/client/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.goodatlas.audiorecord.RNAudioRecordPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.dooboolab.audiorecorderplayer.RNAudioRecorderPlayerPackage;
 import com.facebook.react.ReactInstanceManager;
