1089065f854bee0c83a1b244723b5c36d078f936
2hwayoung <hwa3270@gmail.com>
2021-06-25 18:53:30 +0900

    connects

--- Client/recorder.tsx
+++ Client/recorder.tsx
@@ -72,11 +72,7 @@ export default class Recorder extends Component {
       lon = coords.longitude;
       console.log(lat, lon);
       askResponse = await axios.post(
-<<<<<<< HEAD
-        'http://172.24.122.154:8080/ask',
-=======
-        'http://192.168.35.227:8080/ask',
->>>>>>> 766abe7c (cache delete)
+        'http://10.0.2.2:8080/ask',
           {voice: {data : myVoice},
           coordinates: {lat: lat, lon: lon}},);
       this.setState({
@@ -85,11 +81,7 @@ export default class Recorder extends Component {
       console.log("heee");
       console.log(askResponse.data.voice.text);
       resResponse = await axios.post(
-<<<<<<< HEAD
-        'http://172.24.122.154:8080/response',
-=======
-        'http://192.168.35.227:8080/response',
->>>>>>> 766abe7c (cache delete)
+        'http://10.0.2.2:8080/response',
           {voice: {id : askResponse.data.voice.id},
           coordinates : {id : askResponse.data.coordinates.id}},
       );
